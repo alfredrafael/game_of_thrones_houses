a40109bb73d8f378baa009bd4dec8ec404052980
vmcasbarro <vincenzo.casbarro@gmail.com>
2018-11-21 11:36:15 -0500

    Updates

---
+++ precache-manifest.d53630ce247b10cf59e8e69b16ebdcf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f1e38861f509055b4f90de02945e8f2",
+    "url": "/game_of_thrones_houses/static/media/the-wall.8f1e3886.jpg"
+  },
+  {
+    "revision": "e934fbd93d0eae9173f3",
+    "url": "/game_of_thrones_houses/static/js/runtime~main.e934fbd9.js"
+  },
+  {
+    "revision": "858b61eb04071ceb2faf",
+    "url": "/game_of_thrones_houses/static/js/main.858b61eb.chunk.js"
+  },
+  {
+    "revision": "6ea4476df7c52da542d9",
+    "url": "/game_of_thrones_houses/static/js/1.6ea4476d.chunk.js"
+  },
+  {
+    "revision": "858b61eb04071ceb2faf",
+    "url": "/game_of_thrones_houses/static/css/main.e27071d2.chunk.css"
+  },
+  {
+    "revision": "7c6877da9c573549de07627961092510",
+    "url": "/game_of_thrones_houses/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/game_of_thrones_houses/precache-manifest.d53630ce247b10cf59e8e69b16ebdcf7.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/game_of_thrones_houses/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
